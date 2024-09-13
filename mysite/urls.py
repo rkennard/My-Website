c63a28aff37f2a34edd7dda3b72f377ebcc8865a
Rob <robkennard0@gmail.com>
2024-09-13 14:03:06 -0400

    Changes the HTML for the site.

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,3 +1,6 @@
+from django.contrib import admin
+from django.urls import path, include
+
 """
 URL configuration for mysite project.
 
@@ -19,4 +22,5 @@ from django.urls import path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
 ]
